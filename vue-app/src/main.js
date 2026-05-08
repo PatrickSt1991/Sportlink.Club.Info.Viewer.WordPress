8b47e1aa8f0e412f6285de80db71c05c46935765
PatrickSt1991 <patrick.stel@kpnmail.nl>
2026-05-08 13:01:40 +0200

    css styling fix for CF7 with this plugin

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,5 +1,3 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { applyPersistentBackground } from '@/utils/background';
